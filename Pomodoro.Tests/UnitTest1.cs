870e6600971cfca98a3e423d3bd807ce06cdc882
maylis <maylislb@hotmail.fr>
2024-06-04 15:31:25 +0200

    Add Unit Test

--- Pomodoro.Tests/UnitTest1.cs
+++ Pomodoro.Tests/UnitTest1.cs
@@ -1,10 +1,73 @@
+using Blazored.LocalStorage;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
 namespace Pomodoro.Tests;
 
-public class UnitTest1
+public class IndexTests : TestContext
 {
-    [Fact]
-    public void Test1()
+    public IndexTests()
     {
+        var localStorageMock = new Mock<ILocalStorageService>();
 
+        Services.AddSingleton(localStorageMock.Object);
+    }
+
+    [Fact]
+    public void IndexComponentRendersRemainingTimeCorrectly()
+    {
+        // Arrange
+        var cut = RenderComponent<pomodoro.Pages.Index>();
+
+        // Act
+        cut.Find("button[type='submit']").Click();
+
+        // Assert
+        cut.Find("p").MarkupMatches("<p>Temps total de travail: 0 minutes</p>");
+    }
+
+    [Fact]
+    public void StopPomodoro_ShouldStopTimer()
+    {
+        // Arrange
+        var component = RenderComponent<pomodoro.Pages.Index>();
+        var startButton = component.Find("button[type='submit']");
+        startButton.Click();
+        var stopButton = component.FindAll("button")[1];
+
+        // Act
+        stopButton.Click();
+
+        // Assert
+        Assert.False(component.Instance.isSessionActive);
+        Assert.Equal(0, component.Instance.remainingTime);
+    }
+
+    [Fact]
+    public void IndexComponentRendersCorrectly()
+    {
+        // Arrange
+        var cut = RenderComponent<pomodoro.Pages.Index>();
+
+        // Act
+        cut.Instance.StartPomodoro();
+
+        // Assert
+        cut.Find("p").MarkupMatches("<p>Temps total de travail: 0 minutes</p>");
+    }
+
+    [Fact]
+    public void UpdateWorkTime_ShouldIncreaseTotalWorkMinutes()
+    {
+        // Arrange
+        var component = RenderComponent<pomodoro.Pages.Index>();
+        component.Instance.pomodoroSettings.WorkDuration = 25;
+
+        // Act
+        component.Instance.UpdateWorkTime();
+
+        // Assert
+        Assert.Equal(25, component.Instance.totalWorkMinutes);
     }
 }
